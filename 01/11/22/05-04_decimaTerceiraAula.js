dfc7cb99649d315a49d437802d4b7927244398e8
Pedrolazzarin <pedrolazzarin@gmail.com>
2022-11-01 17:02:05 -0300

    att 1.1

---
+++ 01/11/22/05-04_decimaTerceiraAula.js
@@ -0,0 +1,29 @@
+// parametros de funçao
+
+
+// function soma(numero1, numero2){
+//     return numero1 + numero2;
+// }
+
+// console.log(soma(2, 2))
+// console.log(soma(245, 20))
+// console.log(soma(-500, 60))
+
+// parametros x argumentos
+
+// ordem dos parametros
+
+// function nomeIdade(nome, idade){
+//     return `meu nome eh ${nome} e minha idade eh ${idade}`
+// }
+
+// console.log(nomeIdade("Pedro", "18");
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+function multiplica(numero1 = 1, numero2 = 1){
+    return numero1 * numero2
+}
+
+console.log(multiplica(soma(4,5) ))
---
+++ 01/11/22/05-05_decimaQuartaAula.js
@@ -0,0 +1,22 @@
+// // function cumprimentaPessoa(pessoa){
+// //     console.log(`oi, ${pessoa}!`)
+// //    }
+   
+// //    cumprimentaPessoa('Pedro')
+
+
+// function cumprimentar(){
+//     return 'Oi gente!'
+//    }
+   
+//    function cumprimentaPessoa(nomePessoa) {
+//     console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+//    }
+   
+//    cumprimentaPessoa('Pedro') // “Oi gente! Meu nome é Pedro”
+
+function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+   }
+   
+   console.log(operacaoMatematica(15, 30, 45)) // 90
