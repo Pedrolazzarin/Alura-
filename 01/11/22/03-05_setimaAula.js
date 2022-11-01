1f4131fd31582b988456c4a5429ab89c450118c2
Pedrolazzarin <pedrolazzarin@gmail.com>
2022-11-01 14:22:43 -0300

    Alura2!

---
+++ 01/11/22/03-05_setimaAula.js
@@ -0,0 +1,10 @@
+//log -> registro 
+const minhaVar = true;
+
+// console.log(123)
+// console.log("Eu sou um texto")
+// console.log(minhaVar)
+
+//tratamento de erro 
+
+window.error("deu erro!")
---
+++ 01/11/22/04-02_oitavaaula.js
@@ -0,0 +1,23 @@
+// == -> ( comparaçao implicita ) <> ( so compara o valor )
+
+// const numero = 5;
+// const texto = "5";
+
+// console.log(numero == texto)
+
+// === -> ( comparaçao explicita ) <> ( compara o valor e o tipo de dado )
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto)
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// conversao explicita 
+
+Number()
+String()
+
