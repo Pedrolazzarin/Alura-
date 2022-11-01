e07730946c115aed08355f0ccaa23e747cefb457
Pedrolazzarin <pedrolazzarin@gmail.com>
2022-11-01 15:54:33 -0300

    umNomeNovo

---
+++ 01/11/22/04-05_decimaAula.js
@@ -0,0 +1,8 @@
+const nome = "Pedro";
+const idade = 2022-2004;
+const cidadeDeNascimento = "Cascavel PR";
+//const apresentacao = "Meu nome eh: " + nome + " tenho " + idade + " anos e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `"meu nome eh ${nome}, minha idade eh ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
---
+++ 01/11/22/05-02_decimaSegundaAula.js
@@ -0,0 +1,25 @@
+// let x = "";
+// console.log(x)
+// x= "oi";
+    
+// DECLARACAO DE FUNCAO
+
+// 1- Declara a funçao
+
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+// 2- Executa a funçao (1 ou mais vzs)
+
+imprimeTexto(soma());
+// imprimeTexto("fala meu consagrado");
+
+// tres formas de escrever funcoes
+
+function soma(){
+    return 2 + 2;
+    
+}
+
+// console.log(soma());
