a8dbab662d9d4e75b28e1fa869fdcd93d0118944
Pedrolazzarin <117119439+Pedrolazzarin@users.noreply.github.com>
2022-11-07 13:47:37 -0300

    Delete Desafio-ListaDeTelefones.js

--- Desafio-ListaDeTelefones.js
+++
@@ -1,9 +0,0 @@
-const cliente = {
-    nome:"Andre",
-    idade:36,
-    cpf:"12345678901",
-    email:"andre@email.com",
-    fones:["559123456789", "552132455632"]
-}
-
-cliente.fones.forEach(fone => console.log(fone))
